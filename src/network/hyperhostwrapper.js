358aeef03205aa4e62aab7ca1f2670d9b587bb95
RationalCoding <thomasmullendesign@gmail.com>
2017-04-09 17:32:58 -0400

    fix deploy and #15

--- src/network/hyperhostwrapper.js
+++ src/network/hyperhostwrapper.js
@@ -3,6 +3,11 @@
 // Wraps the HyperHost instance
 // TODO: When HyperHost uses simple-signal, make child of remote.js
 
+var EventEmitter = require('events').EventEmitter
+var inherits = require('inherits')
+
+inherits(HyperHostWrapper, EventEmitter)
+
 function HyperHostWrapper () {
   var self = this
   if (!(self instanceof HyperHostWrapper)) return new HyperHostWrapper()
@@ -10,20 +15,27 @@ function HyperHostWrapper () {
   self._host = new HyperHost()
 }
 
-HyperHostWrapper.prototype.deploy = function (tree, cb) {
+HyperHostWrapper.prototype.deploy = function (tree) {
   var self = this
+  console.log('HH started')
 
   self._host.on('ready', function (url) {
-    cb(url)
+    console.log('HH ready')
+    self.emit('ready', url)
   })
 
   self._host.io.on('digest', function () {
-    console.log('hello world')
+    console.log('HH digested')
     self._host.launch()
   })
 
   console.log(tree)
-  self._host.io.contentTree(tree)
+  try {
+    self._host.io.contentTree(tree) // TODO: Don't try/catch when HH supports "error" event
+  } catch (err) {
+    console.error(err)
+    self.emit('error', err)
+  }
 }
 
 module.exports = new HyperHostWrapper()
